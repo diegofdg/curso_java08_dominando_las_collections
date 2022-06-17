07a11a336b007315fb38973a7588063e007ed1a1
diegofdg <diegofdg@gmail.com>
2022-06-17 08:54:42 -0300

    clase_08-maps

---
+++ 08_maps/src/ar/com/alura/model/Curso.java
@@ -0,0 +1,80 @@
+package ar.com.alura.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+public class Curso {
+	private String nombre;
+	private int tiempo;
+	private List <Aula> aulaList = new ArrayList<>();
+	private Collection<Alumno> alumnos = new HashSet<>();
+	private Map<String, Alumno> alumnoMap = new HashMap<>();
+	
+	public Curso(String nombre, int tiempo) {
+		super();
+		this.nombre = nombre;
+		this.tiempo = tiempo;
+	}
+	
+	public Curso(String nombre, int tiempo, List<Aula> aulaList) {
+		super();
+		this.nombre = nombre;
+		this.tiempo = tiempo;
+		this.aulaList = aulaList;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getTiempo() {
+		return tiempo;
+	}
+	
+	public void setTiempo(int tiempo) {
+		this.tiempo = tiempo;
+	}	
+	
+	public List<Aula> getAulaList() {
+		return Collections.unmodifiableList(aulaList);
+	}
+
+	public void setAulaList(List<Aula> aulaList) {
+		this.aulaList = aulaList;
+	}
+	
+	public void addAula(Aula clase) {
+		this.aulaList.add(clase);
+	}
+	
+	public void addAlumno(Alumno alumno) {
+		this.alumnos.add(alumno);
+		this.alumnoMap.put(alumno.getCodigo(), alumno);
+	}	
+
+    public boolean verificaAlumno(Alumno alumno) {
+    	return this.alumnos.contains(alumno);
+    }
+    
+    public Collection<Alumno> getAlumnos() {
+        return alumnos;
+    }
+
+    public Map<String, Alumno> getAlumnoMap() {
+        return alumnoMap;
+    }
+
+	@Override
+	public String toString() {
+		return this.nombre;
+	}
+}
